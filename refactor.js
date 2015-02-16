19b2756bf7b963d2ad2773c087df18e14d9ada83
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-16 17:11:57 -0500

    completed #9

--- refactor.js
+++ refactor.js
@@ -280,6 +280,13 @@ var twoPlusTwo = addNumbers(2,2);
 
 // Put your answer below -------------------------
 
+var addNumbers = function(numberA, numberB) {
+  var twoPlusTwo = (numberA + numberB);
+  
+  console.log(twoPlusTwo);
+};
+
+
 
 // -----------------------------------------------
 
